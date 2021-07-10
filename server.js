d72db21ae20845d84c22a398b68eb5a3acd43e04
Rafael Fernandes Lopes <rafael.fernandes@gmail.com>
2021-07-09 21:28:12 -0300

    Listagem de produtos implementada

--- server.js
+++ server.js
@@ -4,10 +4,29 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
+const bd = [
+    {
+        produto: "Big Mac",
+        preco: 20.0
+    },
+    {
+        produto: "Coca-cola",
+        preco: 5.0
+    },
+    {
+        produto: "Batata frita",
+        preco: 10.0
+    },
+];
+
 app.use(express.json());
 
 app.get("/", (request, response) => {
     response.send("Alô mundo!");
+});
+
+app.get("/produtos", (req, res) => {
+    res.send(bd);
 });
 
 app.listen(port, () => {
